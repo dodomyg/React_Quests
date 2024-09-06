606491992503e668f1b781fd6844480da0e01e69
dodomyg <sharvil.goverdhan@gmail.com>
2024-09-06 18:40:06 +0530

    Quiz app done

--- src/hooks/Quiz.jsx
+++ src/hooks/Quiz.jsx
@@ -1,61 +1,112 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 const quizData = [
   {
     question: "What is the capital of India?",
     options: [
-      { text: "Delhi", isCorrect: true },
-      { text: "Mumbai", isCorrect: false },
-      { text: "Kolkata", isCorrect: false },
-      { text: "Pune", isCorrect: false },
+      { text: "Delhi", correct: true },
+      { text: "Mumbai", correct: false },
+      { text: "Chennai", correct: false },
+      { text: "Kolkata", correct: false },
     ],
   },
   {
-    question: "What is the national animal of India?",
+    question: "What is the largest country in the world?",
     options: [
-      { text: "Lion", isCorrect: false },
-      { text: "Tiger", isCorrect: false },
-      { text: "Elephant", isCorrect: false },
-      { text: "Cow", isCorrect: false },
+      { text: "Russia", correct: true },
+      { text: "Canada", correct: false },
+      { text: "China", correct: false },
+      { text: "USA", correct: false },
     ],
   },
   {
-    question: "What is the capital of Australia?",
+    question: "What is the smallest continent in the world?",
     options: [
-      { text: "Sydney", isCorrect: false },
-      { text: "Melbourne", isCorrect: false },
-      { text: "Canberra", isCorrect: true },
-      { text: "Perth", isCorrect: false },
-    ],
-  },
-  {
-    question: "What is the national fruit of India?",
-    options: [
-      { text: "Mango", isCorrect: true },
-      { text: "Apple", isCorrect: false },
-      { text: "Orange", isCorrect: false },
-      { text: "Banana", isCorrect: false },
+      { text: "Africa", correct: false },
+      { text: "Antarctica", correct: false },
+      { text: "Australia", correct: true },
+      { text: "South America", correct: false },
     ],
   },
 ];
 
 const Quiz = () => {
-  const [question,displayedQuest]=useState(0);
-  const [selected,setSelected]=useState([])
-  const [answers,setAns]=useState([])
+  const [ans, setAns] = useState([]);
+  const [currQuest, setCurrQuest] = useState(0);
+  const [selected, setSelected] = useState({});
+  const totalScore = quizData.length;
 
-  return <div>
-    <h1>Quiz App</h1>
-    
-    {quizData.slice(question,question+1).map((data,index)=>{
-      return <div key={index}>
-        <h2>{data.question}</h2>
-        {data.options.map((option)=>(
-          <button>{option.text}</button>
-        ))}
+  const selectedAns = (option) => {
+    setSelected(option);
+    if (option.correct) {
+      setAns([...ans, option.text]);
+    }
+  };
+
+  const marks = ans.length.toString() + "/" + totalScore.toString();
+
+  const nextQuest = (currQuest) => {
+    if (currQuest === quizData.length - 1) {
+      return;
+    }
+
+    setCurrQuest(currQuest + 1);
+    setSelected({});
+  };
+
+  return (
+    <div className="max-w-md mx-auto p-5">
+      <h1 className="text-3xl text-center">Quiz Up</h1>
+
+      <div className="bg-blue-gray-800 w-40% h-full text-white p-4 flex items-center justify-center my-2">
+        <h2>
+          {quizData.slice(currQuest, currQuest + 1).map((o, i) => (
+            <div key={i}>
+              <h1 key={i}>{o.question}</h1>
+
+              <div className="grid grid-cols-2 place-items-center gap-2 my-5">
+                {o.options.map((opt) => (
+                  <button
+                    className={`${selected === opt ? "text-blue-500" : ""}`}
+                    onClick={() => selectedAns(opt)}
+                    key={opt.text}
+                  >
+                    {opt.text}
+                  </button>
+                ))}
+              </div>
+              <h3
+                className="text-center cursor-pointer"
+                hidden={currQuest === quizData.length - 1}
+                onClick={() => nextQuest(currQuest)}
+              >
+                Next
+              </h3>
+            </div>
+          ))}
+          {currQuest === quizData.length - 1 && (
+            <button
+              onClick={() => {
+                if (currQuest === quizData.length - 1) {
+                  setCurrQuest(0);
+                  if (marks === "3/3") {
+                    alert("Congratulations! You recieved : " + marks);
+                  } else if (marks === "2/3") {
+                    alert("Not bad! You recieved : " + marks);
+                  } else {
+                    alert("Bahut bakwaas bhai , try again : "+marks);
+                  }
+                  setAns([]);
+                }
+              }}
+            >
+              Submit
+            </button>
+          )}
+        </h2>
       </div>
-    })}
-  </div>;
+    </div>
+  );
 };
 
 export default Quiz;
